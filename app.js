59dab719fb97022ab7dea33fc5df84f6a4341ded
Jozoeroo <joao.nunes.inacio@escola.pr.gov.br>
2025-10-08 08:17:03 -0300

    ok

---
+++ app.js
@@ -0,0 +1,7 @@
+let titulo = documenet.querySelector('h1');
+titulo.innerHTML = 'Jogo do número secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+function verificarChute(){}
