63a2aa758e9fda0059f6526571d8f023b6a3be59
Jozoeroo <joao.nunes.inacio@escola.pr.gov.br>
2025-10-09 11:47:46 -0300

    ok

--- app.js
+++ app.js
@@ -11,6 +11,19 @@ exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 function verificarChute(){
     let chute = document.querySelector('input').value;
     console.log(chute == numeroSecreto);
-}
 
-function
+    if(chute == numeroSecreto){
+        exibirTextoNaTela('h1,'Acertou!)
+        exibirTextoNaTela('p', 'Vocẽ descobriu o número secreto!')
+ } else {
+    if (chute > numeroSecreto){
+       exibirTextoNaTela9('p', 'O número secreto é menor')
+    } else {
+        exibirTextoNaTela('p', 'o número secreto é maior')
+    }
+ }
+}
+function gerearNumeroAletorio(){
+    return parseInt(Math.random() * 10 + 1);   
+
+}
