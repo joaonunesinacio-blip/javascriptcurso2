e1ad8fb1aef62fe1f93d4c599a5ce352b19b95bb
Jozoeroo <joao.nunes.inacio@escola.pr.gov.br>
2025-10-09 09:03:11 -0300

    ok

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-let titulo = documenet.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+let numeroSecreto =  gerearNumeroAletorio();
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = textp;
+}
 
-function verificarChute(){}
+exibirTextoNaTela("h1", 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+function verificarChute(){
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
+}
+
+function
