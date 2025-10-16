f55065d59e60678e698c6a2dac68c2054a6ed514
Jozoeroo <joao.nunes.inacio@escola.pr.gov.br>
2025-10-16 09:03:23 -0300

    ok

--- app.js
+++ app.js
@@ -16,3 +16,37 @@ function exbirMensagemIncial() {
 
 exbirMensagemIncial();rearNumeroAletorio();
 
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+
+if (chute == numeroSecreto){
+    exibirTextoNaTela('h1', 'Acertou!');
+    let palavraTentativa = tentativas > 1 ? 'tentativas' :
+    'tentativa';
+    let mensagemTentativas = `Você descobriu o número secreto com
+    ${tentativas} ${palavraTentativa}!`;
+    exibirTextoNaTela('p1', mensagemTentativas);
+    document.getElementById('reiniciar').removeAttribute
+    ('disabled');
+} else {
+        if (chute > numeroSecreto){
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior')
+        }
+        tentativas++;
+        limparCampo();
+
+    }
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exbirMensagemInciale();
+    document.getElementById('reiniciar').setAttribute('disabled',true);
+}
