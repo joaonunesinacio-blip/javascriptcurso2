abca31a7261186037ef603e9d3e0dcac8267122f
Jozoeroo <joao.nunes.inacio@escola.pr.gov.br>
2025-10-22 08:18:01 -0300

    okl

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ titulo.innerHTML = 'jogo do numero secreto';
 
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'escolha um numero entre 1 e 10';
+
+function verificarChute() {
+    console.log('o botao foi clicado!')
+}
